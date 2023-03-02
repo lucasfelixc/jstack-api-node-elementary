634f319aa2c1ef1faf9d7c57233279c77a8302f0
Lucas Felix <lucasfelixdev@gmail.com>
2023-03-02 20:50:23 -0300

    chore: params validation added and user id route created

--- src/index.js
+++ src/index.js
@@ -1,19 +1,34 @@
 const http = require('http');
+const { URL } = require('url');
 
 const routes = require('./routes');
 
 const server = http.createServer((request, response) => {
-  console.log(`Request method: ${request.method} | Endpoint: ${request.url}`);
+  const parsedUrl = new URL(`http://localhost:3001${request.url}`);
+  console.log(`Request method: ${request.method} | Endpoint: ${parsedUrl.pathname}`);
+
+  let { pathname } = parsedUrl;
+  let id = null;
+
+  const splitEndpoint = pathname.split('/').filter(Boolean);
+
+  if (splitEndpoint.length > 1) {
+    pathname = `/${splitEndpoint[0]}/:id`;
+    id = splitEndpoint[1];
+  }
 
   const route = routes.find((routeObject) => (
-    routeObject.endpoint === request.url && routeObject.method === request.method
+    routeObject.endpoint === pathname && routeObject.method === request.method
   ));
 
   if (route) {
+    request.query = Object.fromEntries(parsedUrl.searchParams);
+    request.params = { id };
+
     route.handler(request, response);
   } else {
     response.writeHead(404, { 'content-type': 'text/html' });
-    response.end(`Cannot ${request.method} ${request.url}`);
+    response.end(`Cannot ${request.method} ${parsedUrl.pathname}`);
   }
 });
 
