aad09632279aedc1c4f53df1f9ac452be143ceb3
Lucas Felix <lucasfelixdev@gmail.com>
2023-03-05 10:28:22 -0300

    chore: update and delete routes created

--- src/routes.js
+++ src/routes.js
@@ -16,4 +16,14 @@ module.exports = [
     method: 'POST',
     handler: UserController.createUser,
   },
+  {
+    endpoint: '/users/:id',
+    method: 'PUT',
+    handler: UserController.udpateUser,
+  },
+  {
+    endpoint: '/users/:id',
+    method: 'DELETE',
+    handler: UserController.deleteUser,
+  },
 ];
